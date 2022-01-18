cb07b44c54f89ce993074250e87cf417b9e50402
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2022-01-18 09:41:24 -0300

    change function input to buffer

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-var mammoth = require("mammoth");
-var pdf = require('html-pdf');
+const mammoth = require('mammoth')
+const pdf = require('html-pdf')
 
-function convertToPdf(inputDocFilePathWithFileName, outputDocFilePathWithFileName, callback) {
-  mammoth.convertToHtml({
-      path: inputDocFilePathWithFileName
-    })
-    .then(function (result) {
-      var html = result.value; // The generated HTML 
-      pdf.create(html).toFile(outputDocFilePathWithFileName, function (err, res) {
-        if (err) {
-          callback(err);
-          console.log(err);
-          return;
-        }
-        callback(null, res);
-      });
-      var messages = result.messages; // Any messages, such as warnings during conversion 
-      console.log(messages);
-    })
-    .done();
+async function convertToPdf(docxFileBuffer) {
+	return new Promise((resolve, reject) => {
+		mammoth
+			.convertToHtml({
+				buffer: docxFileBuffer,
+			})
+			.then(function (result) {
+				const html = result.value // The generated HTML
+				pdf.create(html).toBuffer(function (err, res) {
+					if (err) {
+						reject(err)
+						return
+					}
+					resolve(res)
+				})
+			})
+	})
 }
 
-module.exports = convertToPdf;
+module.exports = convertToPdf
